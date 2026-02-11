1e2be1bef01524c3d7ca3bf72791ac27b496e167
Daniel Allen <dan@aeternagroup.net>
2026-02-11 23:53:04 +0000

    Update to schema version v4.59.0

--- variables.tf
+++ variables.tf
@@ -61,8 +61,8 @@ EOT
     subnet_id                                  = string
     automatically_rotate_key_to_latest_enabled = optional(bool)
     key_vault_key_id                           = optional(string)
-    mtu                                        = optional(number, 1500)
-    ntp_server                                 = optional(string, "time.windows.com")
+    mtu                                        = optional(number) # Default: 1500
+    ntp_server                                 = optional(string) # Default: "time.windows.com"
     tags                                       = optional(map(string))
     default_access_policy = optional(object({
       access_rule = list(object({
